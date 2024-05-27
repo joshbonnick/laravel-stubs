5f22a249f91e16df4470ce10590470f4b67b2a5a
Josh <josh@bonnick.dev>
2024-05-27 14:35:41 +0100

    add strict types to stubs

--- tests/StubStrictTypesTest.php
+++ tests/StubStrictTypesTest.php
@@ -1,3 +1,18 @@
 <?php
 
 declare(strict_types=1);
+
+it('declares strict_types in all the stubs', function () {
+
+    $this->assertDirectoryExists($dir = __DIR__.'/../stubs');
+    $files = scandir($dir);
+
+    foreach ($files as $file) {
+        if ($file === '.' || $file === '..') {
+            continue;
+        }
+        $content = file_get_contents("$dir/$file");
+
+        $this->assertStringContainsString('declare(strict_types=1);', $content, "$file does not use strict types.");
+    }
+});
